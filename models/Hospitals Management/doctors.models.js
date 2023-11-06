c0419fa94b980ff344772963d55b650876ffae42
Hamza-Maghfoor679 <139769570+Hamza-Maghfoor679@users.noreply.github.com>
2023-11-06 21:18:32 +0500

    Initial commit

---
+++ models/Hospitals Management/doctors.models.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const doctorsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: String,
+      required: true,
+    },
+    qualification: {
+      type: String,
+      required: true,
+    },
+    experienceInYear: {
+      type: Number,
+      default: 0,
+    },
+    workPlaces: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Hospital',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Doctor = mongoose.model('Doctor', doctorsSchema);
